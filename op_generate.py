ec6a1fdc846b431ae872b4658585d1fa8f24c0a0
alexnanow <53272937+alexnanow@users.noreply.github.com>
2022-09-16 14:13:59 +0100

    Update op_generate.py
    - Added commands to cleanup IPTABLES
    - Added commands to apply IPTABLES rules

--- op_generate.py
+++ op_generate.py
@@ -14,9 +14,16 @@ def open_ports():
    lports = list(set(lports))
    return lports
 
+print ('Applying iptables and ipset rules')
 os.system('ipset destroy %s' % set_name)
 os.system('ipset create %s bitmap:port range 0-65535' % set_name)
 os.system('ipset add %s 0-65535' % set_name)
+os.system('iptables -F')
+os.system('iptables -t nat -F')
+os.system('iptables -A OUTPUT -p tcp -m tcp --tcp-flags RST RST -j DROP')
+#os.system('iptables -A OUTPUT -p icmp -m icmp --icmp-type 3 -j DROP')
+os.system('iptables -t nat -A PREROUTING -p tcp -m set --match-set %s dst -j REDIRECT --to-ports 8888' % set_name)
+print ('Rules applied')
 
 openports = open_ports()
 for port in openports:
@@ -25,10 +32,8 @@ for port in openports:
 
 
 
-#os.system('iptables -F') os.system('iptables -t nat -F') 
+#os.system('iptables -F') os.system('iptables -t nat -F')
 #os.system('iptables-restore < /etc/iptables.rules')
 
-#iptables -A OUTPUT -p tcp -m tcp --tcp-flags RST RST -j DROP 
+#iptables -A OUTPUT -p tcp -m tcp --tcp-flags RST RST -j DROP
 #iptables -t nat -A PREROUTING -p tcp -m set --match-set rports dst -j REDIRECT --to-ports 81
-
-
